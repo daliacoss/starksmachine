bfea256c5730ddcf78114a6da67e0de6cffd801f
david <cosstropolis@gmail.com>
2014-06-16 03:14:08 -0400

    first commit

---
+++ static/js/stark.js
@@ -0,0 +1,116 @@
+/*
+a single operator (wrapper for an oscillator)
+*/
+function Operator(context){
+	this.oscillator = context.createOscillator();
+	this.velocity = context.createGain();
+	this.oscillator.connect(velocity);
+}
+
+/*
+a monophonic, multi-oscillator instrument
+
+context (AudioContext): the AudioContext to place the instrument in
+numOperators (int): number of operators (oscillators)
+masterVolume (float, default=0.5): the starting volume (0.0 - 1.0)
+*/
+function Stark(context, numOperators, masterVolume){
+
+	// connections:
+	// [operators] -> [amps] -> velocity -> master amp -> destination
+
+	this.context = context;
+	this.operators = [];
+	this.amps = [];
+	this.velocity = context.createGain();
+	this.masterAmp = context.createGain();
+
+	for (var i=0; i<numOperators; i++){
+		var op = context.createOscillator();
+		var amp = context.createGain();
+
+		op.connect(amp);
+		amp.connect(this.velocity);
+
+		op.frequency.value = 440;
+		amp.gain.value = !i * 1;
+
+		this.operators.push(op);
+		this.amps.push(amp);
+
+		op.start(0);
+	}
+
+	this.velocity.connect(this.masterAmp);
+	this.velocity.gain.value = 0;
+	this.masterAmp.connect(context.destination);
+	// allow 0 volume, but not false or null
+	this.masterAmp.gain.value = (masterVolume === 0) ? 0 : masterVolume || 0.2;
+
+	this.pitchClassesSorted = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+	this.pitchClassFreqs = {
+		"C": 8.17580,
+		"C#": 8.66196,
+		"D": 9.17702,
+		"D#": 9.72272,
+		"E": 10.30086,
+		"F": 10.91338,
+		"F#": 11.56233,
+		"G": 12.24986,
+		"G#": 12.97827,
+		"A": 13.75000,
+		"A#": 14.56762,
+		"B": 15.43385,
+	};
+}
+
+/*
+return associated frequency of a pitch class
+
+pitch (int/string): absolute pitch index (int) or pitch class name (string)
+octave (int, default=0): octave if pitch is string
+*/
+Stark.prototype.pitchToFrequency = function(pitch, octave){
+
+	var pitchClass;
+	if (typeof pitch == "string"){
+		pitchClass = pitchClassFreqs[pitch];
+		octave = octave || 0;
+	}
+	else {
+		pitchClass = this.pitchClassesSorted[pitch % this.pitchClassesSorted.length];
+		octave = Math.floor(pitch / this.pitchClassesSorted.length);
+	}
+
+	return this.pitchClassFreqs[pitchClass] * Math.pow(2, octave);
+}
+
+/*
+begin a note event
+
+pitch (int): absolute pitch index (including octave)
+velocity (float, default=1.0): 0.0 - 1.0
+*/
+Stark.prototype.noteOn = function(pitch, velocity){
+	this.noteOnHz(this.pitchToFrequency(pitch), velocity);
+}
+
+/*
+begin a note event
+
+frequency (float): frequency in Hertz
+velocity (float, default=1.0): 0.0 - 1.0
+*/
+Stark.prototype.noteOnHz = function(freq, velocity){
+	for (var i=0; i<this.operators.length; i++){
+		this.operators[i].frequency.setValueAtTime(freq, this.context.currentTime);
+	}
+	this.velocity.gain.value = velocity;
+}
+
+/*
+stop a note event
+*/
+Stark.prototype.noteOff = function(){
+	this.velocity.gain.value = 0;
+}
---
+++ static/js/starksmachine.js
@@ -0,0 +1,57 @@
+function StarksMachine(){
+	this.context = new AudioContext();
+	this.instrument = new Stark(this.context, 2);
+	this.instrument.operators[0].type = "square";
+
+	this._currentNote = -1;
+	this._currentInterval;
+}
+
+StarksMachine.prototype.play = function(x, y, width, height){
+	var pitch = Math.floor((x / width) * 64) + 32;
+	if (this._currentNote != pitch){
+		this._currentNote = pitch;
+		this.instrument.noteOn(pitch, 1);
+	}
+}
+
+StarksMachine.prototype.mousedown = function(event){
+	if (event.which != 1){
+		return;
+	}
+
+	// var pitch = Math.floor(event.clientX / $(window).width() * 64) + 32;
+	// this._currentNote = pitch;
+	// this.instrument.noteOn(pitch, 1);
+	this.play(event.clientX, null, $(window).width());
+
+}
+
+StarksMachine.prototype.mouseup = function(event){
+	this.instrument.noteOff();
+	clearInterval(this._currentInterval);
+	this._currentNote = -1;
+}
+
+StarksMachine.prototype.mousemove = function(event){
+	if (this._currentNote == -1){
+		return;
+	}
+	this.mousedown(event);
+}
+
+window.onload = function(){
+	window.machine = new StarksMachine();
+}
+
+$(window).mousedown(function(event){
+	window.machine.mousedown(event);
+});
+
+$(window).mouseup(function(event){
+	window.machine.mouseup(event);
+});
+
+$(window).mousemove(function(event){
+	window.machine.mousemove(event);
+});
